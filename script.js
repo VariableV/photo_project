f187a8700aefbe3b4af07b3c6bf05fc8afe2e1cb
VariableV <student.mhuss@gmail.com>
2019-05-02 15:08:01 -0500

    intial commmit

---
+++ script.js
@@ -0,0 +1,99 @@
+// Word (pts) : Unit - Explanation
+var data = {"pilatus_bottom":[
+"What is Enviro - Ecosystem services (4 pts) : The Swiss alps are home to many different types of ecosystem services, such as air purification through the trees, and room for habitats. This also includes the natural beauty of the vast mountain range as pictured.",
+"What is Enviro - Natural resources (2 pts) : Switzerland is filled with lots of timber, which is an essential natural resources. In the picture, the availability of trees for harvest are depicted.",
+"What is Enviro - Globalization (4 pts) : As globalization is rapidly occurring, the tourism associated with this is also increasing. In the case of the picture, this is present with the ever growing cities located in the background by the lake, where the local cities are expanding with tourist hotspots.",
+"Ecology - Genes (5 pts) : In the picture the evergreen trees are adapted to survive in the cold weather. This is because the genes of the tree have changed to fit their environment to allow its species to reproduce. Therefore the genes allowed them to exist within the picture.",
+"Ecology - Evolution (5 pts) : The pine trees in the picture have undergone evolution that puts their species at an advantage to survive in the cold. The bark of the trees are more thick in order to insulate more heat, they have flexible branches to counteract the effects of heavy snowfall, and the needles can photosynthesize with little amounts of sunlight.",
+"Ecology - Organism (1 pt) : The most prominent organism pictured in the image are the pine trees.",
+"Ecology - Species (1 pt) : On the surface level there is not an apparent richness of species, but from the image we can see a species of evergreen trees, Pinus, i.e. Pine.",
+"Ecology - Population (1 pt) : In the picture there is a large population of pine trees.",
+"Ecology - Abiotic (1 pt) : The ecosystem presented in the image contains both biotic and abiotic components. The air and mountains that are pictured would be considered the abiotic parts of the ecosystem as they are both non-living.",
+"Ecology - Biotic (1 pt) : The biotic components of the Swiss alps are present within the image as depicted by the pine trees: front and center.",
+"Ecology - Producers (1 pt) : Pine trees make their own food from inorganic matter, i.e. carbon dioxide and water.",
+"Ecology - Autotrophs (1 pt) : The plants, namely the pines produce their own food from sunlight, carbon dioxide, and water, classifying them as autotrophs.",
+"Ecology - Photosynthesis (1 pt) : The pine trees pictured in the image produce food for themselves through the process of photosynthesis.",
+"Human pop - Rural area (2 pts) : Mount Pilatus (pictured) is located in the rural area of Lucerne, Switzerland. ",
+"Ecology - Biomass (2 pt) : The majority of the biomass in the area is made up of pine trees."
+],
+"ouachita":[
+"Climate - Deciduous Forest (1 pt) : The Ouachita National Forest, pictured in the image, is a deciduous forest located in the southern part of the United States.",
+"Climate - National Forest System (5 pts) : Since the Ouachita mountains are classified as a National Forest, the area is under the jurisdiction of the federal National Forest System.",
+"Climate - Old growth forest (4 pts) : Looking to the left of the image, there is a clear presence of an old growth forest, as indicated by the non-homogenous growth and lining of the trees.",
+"Climate - Multiple-Use lands (5 pts) : In the very front of the image there is a clearing, indicating that timber was likely harvested for commercial usage. The forest also serves as a recreational hotspot, therefore the lands have multiple-uses."
+],
+"hoover_dam":[
+"Water - Dams/Levee (3 pts) : In the middle, the Hoover Dam sits, redirecting the flow of water in order to generate electricity.",
+"Water - Euphotic Zone (4 pts) : The surface of the water is visible in the middle of the image. This visible surface part where light is able to penetrate is known as the “euphotic zone”.",
+"Water - Evaporation (2 pts) : On a hot day, such as the one when the picture was taken, evaporation rates are heightened, therefore more water ends up leaving Lake Mead resulting in smaller amounts of water.",
+"Water - Precipitation (1 pts) : Precipitation is not common in the desert region where Hoover Dam is located, explaining the lack of lush greenery.",
+"Water - Nonconsumptive use (5 pts) : The usage of water in Hoover Dam would be considered “non consumptive” because the water that is run through the turbines that generate electricity is immediately restored into the stream of water it was taken from.",
+"Geology - Hydroelectric energy (5 pts) : As pictured, the hoover dam is a source of hydroelectric energy as the water is channeled through turbines and generators that convert water potential to energy."
+],
+"grand_canyon":[
+"Geology - Physical weathering (1 pt) : Millions of years of physical weathering shaped the grand canyon into what is visible today.",
+"Geology - Chemical weathering (2 pts) : Physical weathering was assisted by the effects of chemical weathering in order to shape the magnificent Grand Canyon.",
+"Geology - Sedimentary rock (1 pt) : In the picture, the sedimentary layers of the Grand Canyon are visible.",
+"Geology - Mineral resources (3 pts) : The Grand Canyon is filled with an innumerable amount of untapped mineral resources that were formed over the course of millions of years.",
+"Geology - Rock cycle (3 pts) : The rock cycle dictates the formation of sedimentary rock. In this picture, evidence of the rock cycle would be in the formation of the large plateau of sedimentary rock."
+],
+"calculator":[
+"Waste - Mercury (5 pts) : This old calculator is made up of electronic parts that contain small amounts of mercury and other heavy metals.",
+"Waste - Non Biodegradable (2 pts) : The plastics that make up the calculator are not biodegradable, not breaking up.",
+"Waste - Lead (4 pts) : Lead is an essential part of electronic devices, and since the calculator is a digital one, there is very large probability its electronic constituents contain trace amounts of lead.",
+"Waste - E Waste (3 pts) : When the calculator becomes completely useless, it will turn into e-waste because the device is made up of electronic parts containing heavy metals.",
+"Waste - Plastic (1 pt) : The calculator in the picture is made mostly of plastic",
+"Human pop - Industrial revolution (3 pts) : The calculator is a result of advanced industrial processes that resulted from the industrial revolution.",
+"Human pop - Carcinogens (3 pts) : The calculator is made up of lots of heavy metals which are typically carcinogens such as lead and mercury.",
+"What is Enviro - External cost (5 pts) : Although the calculator was relatively cheap, the external cost of mining the metals and synthesizing the plastic from petroleum has a much greater external cost. All of which is exerted on nature."
+],
+"swiss_village":[
+"Human pop - Developed country (3 pts) : The consistent modern housing and good infrastructure pictured indicates that the country is most definitely developed (country is Switzerland).",
+"Human pop - Affluence (4 pts) : Due to the country's developed nature, as present through the good infrastructure and modern housing, the country must also therefore be affluent."
+],
+"roadside":[
+"Agriculture - Monocropping (2 pts) : In the picture, there is a presence of one type of crop, in an orderly manner indicating that the land overseers are monocropping.",
+"Agriculture - Soil (1 pt) : The soil in this picture is very light colored and looking dry.",
+"Agriculture - Sand (1 pt) : Due to the light color of the soil, the prominent material must be a mixture of sand and clay.",
+"Agriculture - Gravel (1 pt) : The road that I’m driving on in the picture is mostly made up of gravel.",
+"Agriculture - Clay (2 pts) : The lighter color of the soil indicates a heavy presence of sand, however the land would be a complete desert if the clay was not around to hold water.",
+"Agriculture - Soil porosity (4 pts) : As indicated by the picture, the soil type seems to be a mixture of sand and clay. Therefore the soil porosity should be in the middle, leaning towards porous as the land is very dry and relatively infertile.",
+"Agriculture - Soil permeability (4 pts) : Due to the soil having a relatively high concentration of sand, the permeability is very high."
+],
+"airplanes":[
+"Pollution - Primary pollutant (2 pts) : The engines pictured on the jet planes release primary pollutants when burning kerosene.",
+"Pollution - Secondary pollutant (2 pts) : As a result of many of the jets in the picture flying, lingering pollution would have no source that forms as a result of chemical reaction from primary pollutants.",
+"Pollution - Positive feedback loop (5 pts) : When the jet fuel for the planes are burned carbon dioxide is released, further escalating the greenhouse effect. This warms up the earth even more, causing ice melt and release of more greenhouse gas.",
+"Pollution - Emission (1 pt) : When the planes are in the air, the jet fuel burned releases emissions into the atmosphere.",
+"Pollution - Kyoto protocol (5 pts) : The contrails that form as a result of aviation emissions of planes pictured in the image are required to be regulated by the kyoto protocol in order to lower the carbon dioxide emissions. "
+]};
+
+function handle(name){
+	var html = `
+<img src = "`+name+`.JPG" width = "55%" height="82.5%" style="margin-left: -20px; margin-top: -20px;"/>
+<ul style="width: 55%; height: 82.5%; margin-top: -20px; margin-right: -20px; overflow: auto">
+	`
+	for(var i = 0; i < data[name].length; i++){
+		html += "<li>"+data[name][i]+"</li>";
+	}
+	document.getElementById("modify").innerHTML = html;
+}
+
+function addImage(name){
+	var html = `
+<div id="holder">
+	<center><img src = "` + name + `.JPG" width = "100%" height="100%" class="trigger" href="1" onclick="handle('`+name+`')"/></center>
+</div>`;
+	document.getElementById("images").innerHTML += html;
+}
+
+$( document ).ready(function() {
+	for(var s in data){
+		addImage(s);
+	}
+	$('.trigger').click(function() {
+		$('.modal-wrapper').toggleClass('open');
+		$('.page-wrapper').toggleClass('blur');
+		return false;
+	});
+});
